ea96134fdfcf59f701a294470c48b454883b9a6d
SAZO54 <zosaro2126@gmail.com>
2024-10-06 21:31:36 +0900

    Implemented to support validation when registering product information.

--- src/components/Base/ExhibitModal.vue
+++ src/components/Base/ExhibitModal.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted, defineEmits, defineExpose, onBeforeMount } from 'vue';
 import * as bootstrap from 'bootstrap';
 import axios from 'axios';
+import * as yup from 'yup';
 
 const exhibitName = ref('');
 const minPrice = ref(0);
@@ -31,6 +32,82 @@ defineProps({
   }
 })
 
+
+/**
+ * validation
+ */
+const exhibitNameError = ref('');
+const minPriceError = ref('');
+const currentPriceError = ref('');
+const exhibitQuantityError = ref('');
+const platformError = ref('');
+const currencyError = ref('');
+
+const scheme = yup.object({
+  exhibitName: yup.string().max(256, 'Enter no more than 256 characters.').required('Display Name is required.'),
+  minPrice: yup.number().typeError('Min Price is a required selection.'),
+  currentPrice: yup.number().typeError('Current Exhibit Price is a required selection.'),
+  exhibitQuantity: yup.number().typeError('Exhibit Quantity in stock must be a number.').max(100, 'Enter no more than 100.').required('Exhibit Quantity in stock is required.'),
+  platform: yup.number().required('Storage Space is a required selection.'),
+  currency: yup.number().typeError('Currency is a required selection.'),
+});
+
+async function onSubmitValidate(): Promise<boolean> {
+  // Reset error message
+  exhibitNameError.value = '';
+  minPriceError.value = '';
+  currentPriceError.value = '';
+  exhibitQuantityError.value = '';
+  platformError.value = '';
+  currencyError.value = '';
+
+  try {
+    // Validate input values
+    await scheme.validate({
+      exhibitName: exhibitName.value,
+      minPrice: minPrice.value,
+      currentPrice: currentPrice.value,
+      exhibitQuantity: exhibitQuantity.value,
+      platform: platform.value,
+      currency: currency.value,
+    }, { abortEarly: false });
+
+    return true; // バリデーション成功
+  } catch (err) {
+    // Check if error is an instance of Yup.ValidationError
+    if (err instanceof yup.ValidationError) {
+      // Set error messages for each field
+      err.inner.forEach((validationError) => {
+        switch (validationError.path) {
+          case 'exhibitName':
+            exhibitNameError.value = validationError.message;
+            break;
+          case 'minPrice':
+            minPriceError.value = validationError.message;
+            break;
+          case 'currentPrice':
+            currentPriceError.value = validationError.message;
+            break;
+          case 'exhibitQuantity':
+            exhibitQuantityError.value = validationError.message;
+            break;
+          case 'platform':
+            platformError.value = validationError.message;
+            break;
+          case 'currency':
+            currencyError.value = validationError.message;
+            break;
+          default:
+            console.error('Unexpected validation path:', validationError.path);
+        }
+      });
+    } else {
+      console.error('Unexpected error:', err);
+    }
+    return false; // バリデーション失敗
+  }
+}
+
 /**
  * 商品項目_取得
  */
@@ -51,7 +128,14 @@ async function getProductItems(): Promise<void> {
   }
 }
 
-function addExhibit(): void {
+async function addExhibit(): Promise<void> {
+  const isValid: boolean = await onSubmitValidate();
+    if (!isValid) {
+      // バリデーションエラーがある場合は、処理を終了する
+      console.log('Validation failed.');
+      return;
+    }
+
   emits('addExhibit', {
     exhibitName: exhibitName.value,
     currency: currency.value,
@@ -118,8 +202,8 @@ defineExpose({
           <div class="modal-body">
             <form>
               <div class="mb-3">
-                <label for="inputQuantityStock" class="form-label wid-175">Exhibit Platform</label>
-                <select v-model="platform" class="form-control" id="platformSelect" :disabled="disabled">
+                <label for="inputQuantityStock" class="form-label wid-175">Exhibit Platform<span class="asterisk">*</span></label>
+                <select v-model="platform" class="form-control" id="platformSelect" :disabled="disabled" :class="{ 'is-invalid': platformError }">
                   <option v-for="platformItem in productItemsArray?.platform_data" 
                           :value="platformItem.exhibitor_platform_id" 
                           :key="platformItem.exhibitor_platform_id">
@@ -127,13 +211,15 @@ defineExpose({
                   </option>
                 </select>
               </div>
+              <span class="error-message-modal">{{ platformError }}</span>
               <div class="mb-3">
-                <label for="exhibitName" class="form-label">Display Name</label>
-                <textarea v-model="exhibitName" rows="3" class="form-control" id="exhibitName" :disabled="disabled" placeholder="Enter exhibit name"></textarea>
+                <label for="exhibitName" class="form-label">Display Name<span class="asterisk">*</span></label>
+                <textarea v-model="exhibitName" rows="3" class="form-control" id="exhibitName" :disabled="disabled" placeholder="Enter exhibit name" :class="{ 'is-invalid': exhibitNameError }"></textarea>
               </div>
+              <span class="error-message-modal">{{ exhibitNameError }}</span>
               <div class="mb-3">
                 <label for="currency" class="form-label">Currency</label>
-                <select v-model="currency" class="form-control" id="currencySelect" :disabled="disabled">
+                <select v-model="currency" class="form-control" id="currencySelect" :disabled="disabled" :class="{ 'is-invalid': currencyError }">
                   <option v-for="currencyItem in productItemsArray?.currency_data" 
                           :value="currencyItem.money_currency_id" 
                           :key="currencyItem.money_currency_id">
@@ -141,18 +227,22 @@ defineExpose({
                   </option>
                 </select>
               </div>
+              <span class="error-message-modal">{{ currencyError }}</span>
               <div class="mb-3">
                 <label for="minPrice" class="form-label">Min Price</label>
-                <input v-model="minPrice" type="number" class="form-control" id="minPrice" :disabled="disabled" placeholder="Enter min price" min="0" />
+                <input v-model="minPrice" type="number" class="form-control" id="minPrice" :disabled="disabled" placeholder="Enter min price" min="0" :class="{ 'is-invalid': minPriceError }" />
               </div>
+              <span class="error-message-modal">{{ minPriceError }}</span>
               <div class="mb-3">
                 <label for="currentPrice" class="form-label">Current Exhibit Price</label>
-                <input v-model="currentPrice" type="number" class="form-control" id="currentPrice" :disabled="disabled" placeholder="Enter current exhibit price" min="0" />
+                <input v-model="currentPrice" type="number" class="form-control" id="currentPrice" :disabled="disabled" placeholder="Enter current exhibit price" min="0" :class="{ 'is-invalid': currentPriceError }" />
               </div>
+              <span class="error-message-modal">{{ currentPriceError }}</span>
               <div class="mb-3">
-                <label for="exhibitQuantity" class="form-label">Number of Exhibits</label>
-                <input v-model="exhibitQuantity" type="number" class="form-control" id="exhibitQuantity" :disabled="disabled" placeholder="Enter exhibit quantity" min="1" />
+                <label for="exhibitQuantity" class="form-label">Number of Exhibits<span class="asterisk">*</span></label>
+                <input v-model="exhibitQuantity" type="number" class="form-control" id="exhibitQuantity" :disabled="disabled" placeholder="Enter exhibit quantity" min="1" :class="{ 'is-invalid': exhibitQuantityError }" />
               </div>
+              <span class="error-message-modal">{{ exhibitQuantityError }}</span>
             </form>
           </div>
           <div class="modal-footer" v-if="!disabled">
